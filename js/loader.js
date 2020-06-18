26bffab01de0bdeb4819b43acf7986a59cb52c91
Moussa Ibrahim <moussaebrahem32@gmail.com>
2020-06-18 21:03:58 +0200

    Add webGazerJS

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,11 @@
+//Loader UI
 const loader = document.querySelector('.loader-bg');
+//Hide Loader when page loaded
 window.onload = () =>{
   loader.remove();
 };
+
+//Show Gazer Settings on click
+document.querySelector('.modal-settings').addEventListener('click',() =>{
+  $('#myModal').modal('show');
+});
