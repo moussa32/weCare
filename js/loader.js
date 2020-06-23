302895a0939f2aba2fdec4cc4963d812fb2634eb
Moussa Ibrahim <moussaebrahem32@gmail.com>
2020-06-23 21:16:10 +0200

    Remove webgazerJS

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,4 @@
-//Loader UI
 const loader = document.querySelector('.loader-bg');
-//Hide Loader when page loaded
 window.onload = () =>{
   loader.remove();
 };
-
-//Show Gazer Settings on click
-document.querySelector('.modal-settings').addEventListener('click',() =>{
-  $('#myModal').modal('show');
-});
